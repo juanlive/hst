f4628dca4f71b3c349364f85482bc8f4d260822a
juanlive <juanlive@gmail.com>
2019-04-09 14:41:56 -0300

    Nets setted in Truffle config as in Snowflake tests

--- truffle-config.js
+++ truffle-config.js
@@ -1,3 +1,6 @@
+const gas = 6.5 * 1e6
+const gasPrice = 2000000000 // 2 gwei
+
 module.exports = {
   networks: {
     development: {
@@ -13,6 +16,24 @@ module.exports = {
       gas: 0xfffffffffff,
       gasPrice: 0x01,
       websockets: true
+    },
+    rinkebyIPC: {
+      host: 'localhost',
+      port: 8545,
+      network_id: 4,
+      timeoutBlocks: 200,
+      gas: gas,
+      gasPrice: gasPrice,
+      skipDryRun: true
+    },
+    mainIPC: {
+      host: 'localhost',
+      port: 8545,
+      network_id: 1,
+      timeoutBlocks: 200,
+      gas: gas,
+      gasPrice: gasPrice,
+      skipDryRun: true
     }
   },
   compilers: {
@@ -27,26 +48,6 @@ module.exports = {
     }
   }
 }
-
-/**
- * Use this file to configure your truffle project. It's seeded with some
- * common settings for different networks and features like migrations,
- * compilation and testing. Uncomment the ones you need or modify
- * them to suit your project as necessary.
- *
- * More information about configuration can be found at:
- *
- * truffleframework.com/docs/advanced/configuration
- *
- * To deploy via Infura you'll need a wallet provider (like truffle-hdwallet-provider)
- * to sign your transactions before they're sent to a remote public node. Infura API
- * keys are available for free at: infura.io/register
- *
- * You'll also need a mnemonic - the twelve word phrase the wallet uses to generate
- * public/private key pairs. If you're publishing your code to GitHub make sure you load this
- * phrase from a file you've .gitignored so it doesn't accidentally become public.
- *
- */
 
 // const HDWalletProvider = require('truffle-hdwallet-provider');
 // const infuraKey = "fj4jll3k.....";
@@ -66,45 +67,38 @@ module.exports = {
    */
 
   networks: {
-    // Useful for testing. The `development` name is special - truffle uses it by default
-    // if it's defined here and no other network is specified at the command line.
-    // You should run a client (like ganache-cli, geth or parity) in a separate terminal
-    // tab if you use this network and you must also set the `host`, `port` and `network_id`
-    // options below to some value.
-    //
-    // development: {
-    //  host: "127.0.0.1",     // Localhost (default: none)
-    //  port: 8545,            // Standard Ethereum port (default: none)
-    //  network_id: "*",       // Any network (default: none)
-    // },
-
-    // Another network with more advanced options...
-    // advanced: {
-      // port: 8777,             // Custom port
-      // network_id: 1342,       // Custom network
-      // gas: 8500000,           // Gas sent with each transaction (default: ~6700000)
-      // gasPrice: 20000000000,  // 20 gwei (in wei) (default: 100 gwei)
-      // from: <address>,        // Account to send txs from (default: accounts[0])
-      // websockets: true        // Enable EventEmitter interface for web3 (default: false)
-    // },
-
-    // Useful for deploying to a public network.
-    // NB: It's important to wrap the provider as a function.
-    // ropsten: {
-      // provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/${infuraKey}`),
-      // network_id: 3,       // Ropsten's id
-      // gas: 5500000,        // Ropsten has a lower block limit than mainnet
-      // confirmations: 2,    // # of confs to wait between deployments. (default: 0)
-      // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-      // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
-    // },
-
-    // Useful for private networks
-    // private: {
-      // provider: () => new HDWalletProvider(mnemonic, `https://network.io`),
-      // network_id: 2111,   // This network is yours, in the cloud.
-      // production: true    // Treats this network as if it was a public net. (default: false)
-    // }
+      development: {
+      host: 'localhost',
+      port: 8545,
+      network_id: '*',
+      websockets: true
+    },
+    coverage: {
+      host: 'localhost',
+      port: 8555,
+      network_id: '*',
+      gas: 0xfffffffffff,
+      gasPrice: 0x01,
+      websockets: true
+    },
+    rinkebyIPC: {
+      host: 'localhost',
+      port: 8545,
+      network_id: 4,
+      timeoutBlocks: 200,
+      gas: gas,
+      gasPrice: gasPrice,
+      skipDryRun: true
+    },
+    mainIPC: {
+      host: 'localhost',
+      port: 8545,
+      network_id: 1,
+      timeoutBlocks: 200,
+      gas: gas,
+      gasPrice: gasPrice,
+      skipDryRun: true
+    }
   },
 
   // Set default mocha options here, use special reporters etc.
