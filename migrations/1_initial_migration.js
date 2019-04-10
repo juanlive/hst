bb5187dad3534745252c8f218bd2147a36e063ba
juanlive <juanlive@gmail.com>
2019-04-10 11:08:19 -0300

    Minor changes

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,6 +1,30 @@
 const HSTIssuer = artifacts.require('./HSTIssuer.sol')
+const AddressSet = artifacts.require('./_testing/AddressSet/AddressSet.sol')
+const IdentityRegistry = artifacts.require('./_testing/IdentityRegistry.sol')
+
+const HydroToken = artifacts.require('./_testing/HydroToken.sol')
+
+const SafeMath = artifacts.require('./zeppelin/math/SafeMath.sol')
+const Snowflake = artifacts.require('./Snowflake.sol')
+// const Status = artifacts.require('./resolvers/Status.sol')
+
+const StringUtils = artifacts.require('./resolvers/ClientRaindrop/StringUtils.sol')
+const ClientRaindrop = artifacts.require('./resolvers/ClientRaindrop/ClientRaindrop.sol')
+const OldClientRaindrop = artifacts.require('./_testing/OldClientRaindrop.sol')
+
 
 module.exports = function(deployer) {
+	await deployer.deploy(AddressSet)
+  deployer.link(AddressSet, IdentityRegistry)
+
+  await deployer.deploy(SafeMath)
+  deployer.link(SafeMath, HydroToken)
+  deployer.link(SafeMath, Snowflake)
+
+  await deployer.deploy(StringUtils)
+  deployer.link(StringUtils, ClientRaindrop)
+  deployer.link(StringUtils, OldClientRaindrop)
+
 	await deployer.deploy(HSTIssuer)
   	
 };
